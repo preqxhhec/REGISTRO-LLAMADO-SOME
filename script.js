1a1f91ff998e0e3d2a83200ba9b8b7cb21f4fd6d
preqxhhec <jefeprequirurgico.hhec@gmail.com>
2025-07-07 17:07:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,108 @@
+
+ 
+ 
+  
+
+  function setFechaHoraActual() {
+    const hoy = new Date();
+    const dia = String(hoy.getDate()).padStart(2, '0');
+    const mes = String(hoy.getMonth() + 1).padStart(2, '0'); // Los meses empiezan desde 0
+    const año = hoy.getFullYear();
+    const horas = String(hoy.getHours()).padStart(2, '0');
+    const minutos = String(hoy.getMinutes()).padStart(2, '0');
+    const segundos = String(hoy.getSeconds()).padStart(2, '0');
+  
+    const fechaHoraActual = `${dia}/${mes}/${año} ${horas}:${minutos}:${segundos}`;
+    document.getElementById('fecha').value = fechaHoraActual;
+  }
+  
+  // Actualizar la fecha y hora cada segundo
+  function iniciarReloj() {
+    setInterval(setFechaHoraActual, 1000); // Llama a la función cada 1000 ms (1 segundo)
+  }
+  
+  // Establecer la fecha y hora actual al cargar la página
+  window.onload = function () {
+    setFechaHoraActual(); // Establece la fecha y hora inmediatamente
+    iniciarReloj(); // Comienza el reloj en tiempo real
+  };
+
+
+
+
+
+
+
+
+     
+
+
+
+
+ 
+ 
+
+
+
+document.getElementById('BTN').addEventListener('click', function(e) {
+    e.preventDefault();
+    var formData = new FormData(document.getElementById('form'));
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://script.google.com/macros/s/AKfycby_SrYIDf8lLuKAQYhGACVcgzJ5hAJrZW-k-f8SaX8gBLbM1Or0WPC-zsw_r-q9w2B5iA/exec');
+    xhr.reload = function() {
+      document.getElementById('form').reload(); 
+     
+    };
+    xhr.send(formData);
+    alert('¡Registro exitoso!');
+     form.reset();
+  });
+
+
+
+  function verificarClave() {
+    var password = document.getElementById("password").value;
+    if (password === "Minsal") {
+        document.getElementById("form").style.display = "block";
+        document.getElementById("acceso").style.display = "none";
+    } else {
+        alert("Contraseña incorrecta");
+    }
+}
+
+
+
+
+
+
+
+
+const inputFechaIc = document.getElementById('fechaIc');
+
+inputFechaIc.addEventListener('input', (e) => {
+  const fecha = new Date(inputFechaIc.value);
+  const dia = fecha.getDate().toString().padStart(2, '0');
+  const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+  const año = fecha.getFullYear();
+
+  const inputFechaOculta = document.createElement('input');
+  inputFechaOculta.type = 'hidden';
+  inputFechaOculta.name = 'fechaIcFormatted';
+  inputFechaOculta.value = `${dia}/${mes}/${año}`;
+  inputFechaIc.parentNode.appendChild(inputFechaOculta);
+});
+
+const inputFechaAgenda = document.getElementById('fechaAgenda');
+
+inputFechaAgenda.addEventListener('input', (e) => {
+  const fecha = new Date(inputFechaAgenda.value);
+  const dia = fecha.getDate().toString().padStart(2, '0');
+  const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+  const año = fecha.getFullYear();
+
+  const inputFechaOculta = document.createElement('input');
+  inputFechaOculta.type = 'hidden';
+  inputFechaOculta.name = 'fechaAgendaFormatted';
+  inputFechaOculta.value = `${dia}/${mes}/${año}`;
+  inputFechaAgenda.parentNode.appendChild(inputFechaOculta);
+});
